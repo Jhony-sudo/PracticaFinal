ff520558a7049a54856460bcd2261442e6a4169b
Jhony <Jhony.fuentes19@gmail.com>
2021-06-29 21:47:10 -0600

    Clase principal

---
+++ src/Run/Run.java
@@ -0,0 +1,14 @@
+
+package Run;
+import ventanas.VentanaInicio;
+
+public class Run {
+
+  
+    public static void main(String[] args) {
+       VentanaInicio v = new VentanaInicio();
+       v.setVisible(true);
+        
+    }
+    
+}
